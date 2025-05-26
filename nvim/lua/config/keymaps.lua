8e7f60194ad3c7b81d4c94a7ef31db18f236c2df
Abhishek bagdiya <abhishekbagdiya94@gmail.com>
2025-05-26 21:43:39 +0530

    added some quality-of-life keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Add semicolon at the end of the line
+vim.keymap.set("n", "<leader>e;", "A;<Esc>h", { desc = "Add ; at EOL" })
+
+-- Add comma at the end of the line
+vim.keymap.set("n", "<leader>e,", "A,<Esc>h", { desc = "Add , at EOL" })
